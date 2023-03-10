e6de1ab9a5100546a8ec0ed5becbd686c8d712e4
jiezai666 <619089862@qq.com>
2023-03-10 17:28:50 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type User struct {
+	gorm.Model
+	Name      string `gorm:"type:varchar(19);not null"`
+	Telephone string `gorm:"type:varchar(110);not null;unique"`
+	Password  string `gorm:"type:varchar(100);size:240;not null"`
+}
+
+func main() {
+	db := InitDB()
+
+	r := gin.Default()
+	r.POST("/api/auth/register", func(c *gin.Context) {
+		//获取参数
+		name := c.PostForm("name")
+		telephone := c.PostForm("telephone")
+		password := c.PostForm("password")
+		//数据验证
+		if len(telephone) != 11 {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+			return
+		}
+
+		if len(password) < 6 {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能小于6位"})
+			return
+		}
+
+		//如果名称没有传，给一个10位的随机字符串
+		if len(name) == 0 {
+			name = RandomString(10)
+		}
+		log.Println(name, telephone, password)
+		//判断手机号是否存在
+		if isTelephoneExist(db, telephone) {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号已存在"})
+			return
+		}
+		//创建用户
+		newUser := User{
+			Name:      name,
+			Telephone: telephone,
+			Password:  password,
+		}
+		db.Create(&newUser)
+		c.JSON(200, gin.H{
+			"msg": "注册成功",
+		})
+	})
+	panic(r.Run())
+
+}
+
+func isTelephoneExist(db *gorm.DB, telephone string) bool {
+	var users User
+	db.Where("telephone = ?", telephone).First(&users)
+	if users.ID != 0 {
+		return true
+	}
+	return false
+}
+
+func RandomString(n int) string {
+	var letters = []byte("asdfghjklzxcvbnmqwertyuiopASDFGHJKLZXCVBNMQWERTYUIOP")
+	result := make([]byte, n)
+	rand.Seed(time.Now().Unix())
+	for i := range result {
+		result[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(result)
+}
+
+func InitDB() *gorm.DB {
+
+	host := "localhost" //数据库地址
+	port := "3306"      //数据库端口号
+	dbname := "ginessential"
+	username := "root"
+	password := "123456"
+	charset := "utf8mb4"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local",
+		username,
+		password,
+		host,
+		port,
+		dbname,
+		charset)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, //使用单数表名
+		},
+	})
+	if err != nil {
+		panic("failed to connect database, err:" + err.Error())
+	}
+
+	db.AutoMigrate(&User{})
+
+	return db
+}
